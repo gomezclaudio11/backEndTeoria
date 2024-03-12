3c19e53f03e19e16611785e48669b87d6450c97c
gomezclaudio11 <claudiogomez23@gmail.com>
2024-03-11 23:23:16 -0300

    clase 17 mongoDB

--- clase17-MongoDB/index.js
+++ clase17-MongoDB/index.js
@@ -124,3 +124,193 @@ Listar documentos en la colección 'usuarios' nuevamente:
 db.usuarios.find()
 
  */
+/**
+ CRUD
+ Esta estrechamente vinculado a la gestion de datos digitales
+ COMANDO EN MONGODB
+ db.coll.drop() : borra una colección y sus índices respectivos.
+db.dropDatabase() : elimina la base de datos actual.
+db.createCollection("contacts") : crea una colección en forma 
+explícita.
+db.coll.stats() : refleja estadísticas del uso de la base.
+db.coll.storageSize() : tamaño de almacenamiento de la 
+colección.
+db.coll.totalIndexSize() : tamaño total de todos los índices
+ de la colección.
+db.coll.totalSize(): tamaño total en bytes de los datos de 
+la colección más el tamaño de cada índice de la colección.
+db.coll.validate({full: true}) : comprueba la integridad de
+ una colección.
+db.coll.renameCollection("new_coll", true) : renombra una
+ colección, el  2do parámetro para borrar la colección 
+ destino si existe.
+ db.coll.insertOne( {key:value} ) : inserta un documento en
+  la colección.
+db.coll.insert( {key:value} ) : inserta un documento en la 
+colección (en desuso).
+db.coll.insertMany( [ {key:value}, {key:value}, {key:value} ]):
+ inserta un array de documentos la colección en modo Bulk.
+db.coll.findOne() : busca un documento dentro de una colección
+db.coll.find() : busca todos los documentos dentro de una 
+colección.
+db.coll.find( {key:value} ) : busca los documentos dentro de 
+una colección que satisfacen el filtro de búsqueda.
+db.coll.find().pretty() : devuelve todos los documentos 
+conservando el formato de objeto de salida.
+
+CONTADORES
+db.coll.estimatedDocumentCount()
+Devuelve la cantidad total de documentos encontrados en la 
+colección.
+db.coll.countDocuments( {key: val} )
+Devuelve la cantidad de documentos encontrados en la colección
+ (con filtro de query).
+
+ */
+
+ /**FILTROS */
+ /**
+  db.coll.find({key: {$operator:val}})
+  devuelve los documentos segun el operador de filtro 
+  utilizado
+
+$and : Realiza operación AND -> sintaxis: {$and: [ {},{} ] }
+$or : Realiza operación OR -> sintaxis: {$or: [ {},{} ] }
+$lt : Coincide con valores que son menores que un valor 
+    especificado.
+$lte : Coincide con valores menores o iguales a un valor
+     especificado.
+$gt : Coincide con valores mayores a un valor especificado.
+$gte : Coincide con valores mayores o iguales a un valor  
+    especificado.
+$ne : Coincide con valores que no son iguales a un valor  
+    especificado.
+$eq : Selecciona los documentos que son iguales a un valor 
+    especificado.
+
+OPERADPRES PARA FILTROS DE QUERY
+$exists : Selecciona los documentos según la existencia de un
+     campo.
+$in : Selecciona los documentos especificados en un array. 
+sintaxis: {key:{$in: [array of values] } }
+$nin : Coincide con ninguno de los valores especificados 
+      en un array.
+$size : Coincide con el número de elementos especificados.
+$all : Coincide con todos los valores definidos dentro de un 
+    array.
+$elemMatch : Coincide con algún valor definido dentro del 
+    query.
+
+BUSQUEDA AVANZADA
+db.coll.distinct( val )
+devuelve un array con los distintos valores que toma un 
+determinado campo en los documentos de la colección.
+
+db.coll.find({doc.subdoc:value})
+Se utiliza para filtrar subdocumentos.
+
+db.coll.find({name: /^Max$/i})
+filtra utilizando expresiones regulares
+
+SORT LIMIT SKIP
+sort( { campoA: 1 ó -1 , campoB: 1 ó -1 , ... } ) :
+ Especifica el orden en el que la consulta devuelve documentos
+ coincidentes. El ó los campos por los cuales ordena pueden 
+ contener los valores 1 y -1, estableciendo orden ascendente y
+  descendente respectivamente. El orden se evalúa de izquierda
+   a derecha en caso que los valores coincidan.
+
+limit(num): Especifica el número máximo de documentos 
+devueltos.
+skip(offset) : Saltea la cantidad de documentos especificada.
+
+Se pueden utilizar en forma combinada:
+db.Employee.find().skip(2).limit(3).sort({Employeeid:-1})
+
+  */
+ /**UPDATE Y DELETE */
+ /**
+ - db.collection.updateOne(query, update, options)
+query: especifica el filtro de documentos a ser actualizados.
+update: contiene los datos a ser actualizados con sus
+ operadores respectivos: 
+ $set, $unset, $inc, $rename, $mul, $min, $max, etc.
+options: contiene varias opciones para la actualización,
+ entre ellas: 
+upsert (true ó false) : Es una opción para hacer un insert 
+en caso de que el registro no exista.
+
+-db.coll.updateMany(query, update, options)
+Igual que el anterior, pero hace una actualización múltiple en caso de que el filtro de query devuelva varios resultados.
+
+DELETE
+
+db.coll.deleteOne( {key: val} ): Elimina un sólo documento 
+(el primero) que coincide con el filtro especificado.
+db.coll.deleteMany( {key: val} ): Elimina todos los documentos
+ que coinciden con el filtro especificado.
+db.coll.remove( {key: val} ): Elimina documentos de una 
+colección.
+db.coll.findOneAndDelete( filter, options ): Elimina un solo
+ documento según el filtro y los criterios de clasificación.
+  Algunas de las options pueden ser
+- sort: para establecer orden para el filtro
+- projection: para elegir campos de salida.
+  */
+
+/**USUARIOS Y PERMISOS */
+/**
+ Creacion de usuario con permisos y su eliminacion
+ use admin
+ db.createUser({"user": "root", "pwd":"rootpwd", "roles": ["root"]})
+    user: nombre de usuario
+    pwd: contraseña para el usuario
+    roles: arreglo de objetos. Sirve si el usuario tendra
+          acceso a multiples bases de datos, estableciendo 
+          permisos para cada acceso
+ db.dropUser("root") (borrar)
+
+ Es posible crear usuarios y asignarles accesos mediante roles
+ Usuario lector: Lectura de base de datos
+ Usuario escritor: acceso a lectura y escritura de la base
+                  de datos
+
+  db.createUser(
+    {
+      user: "lector",
+      pwd: "12345",
+      roles: [
+        { role: "read", db: "blog" } 
+      ]
+    }
+  )
+
+  La propiedad db es donde se  indica a qué base de datos se
+   le asignará dicho rol
+
+    db.createUser(
+    {
+      user: "escritor",
+      pwd: "12345",
+      roles: [
+        { role: "readWrite", db: "blog" } 
+      ]
+    }
+  )
+
+  - Para poder ingresar al shell de mongo con el usuario
+   usaremos los parámetros -u y -p.
+   mongo -u lector -p12345
+  - Verificamos si podemos leer los posts
+   use blog
+   db.blog.find()
+  - Ahora intentaremos insertar un nuevo documento que nos
+  deberia lanzar un error
+
+  -Para el usuario escritor, primero debemos salir del shell 
+  y volver a loguearnos:
+  mongo -u escritor -p 12345 --authenticationDatabase blog
+
+  Para estas pruebas, es importante ejecutar el servidor en 
+  modo autenticación: usar el comando mongod --auth
+ */
