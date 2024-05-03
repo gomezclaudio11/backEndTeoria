21f1f9745a195c4b8fa7c508c13f729715d3560f
gomezclaudio11 <claudiogomez23@gmail.com>
2024-05-03 18:20:46 -0300

    clase 34 aws

---
+++ clase34-aws/index.js
@@ -0,0 +1,21 @@
+/**PLATAFORMA AWS */
+/**
+ amazon web services
+ Es una plataforma en la nube muy adoptada y completa
+ ELASTIC BEANSTALK
+ para implementar aplicaciones nodejs en la nube
+ Pide datos de tarjeta de credito
+ Administra de manera automatica la implementacion de nuestra
+ aplicacion (desde el aprovisionamiento de la capacidad, el 
+    balanceo de carga y el auto escalamiento hasta la monitorizacion
+    del estado) ingresando unicamente el codigo
+Auto scaling para ajuste de escalado automatico
+
+Amazon DynamoDb
+es un servicio de base de datos noSql rapido y flexible
+completamente administrado en la nube
+ccompatible con modelos de almacenamiento de valor de clave
+y de documentos
+
+COMO CONFIGURAR EL PROYECTO VER LAS DIAPOSITIVAS
+ */
