030116a26ec64928cdbda89e1a6e58d1cff46fde
gomezclaudio11 <claudiogomez23@gmail.com>
2024-05-06 23:39:00 -0300

    clase 36 twilio whatsapp OWASP

---
+++ clase36-twiliowhatsapp/index.js
@@ -0,0 +1,28 @@
+/**TWILIO WHATSAPP */
+/**
+ se puede enviar y recibir msjs de whatsapp tambien mediante
+  twilio
+ funciona como intermediario entre la cuenta de wharsapp y 
+ nosotros
+ entramos a twilio para whatsapp para ver la docu
+
+ *Ingresamos de nuevo a la cuenta de Twilio que nos creamos 
+ la última actividad, volvemos al menú de mensajes, y 
+ activamos nuestro sandbox (espacio de pruebas).
+VER DIAPO
+
+ */
+
+/**OWASP */
+/**
+ open web application security project
+
+ Es un proyecto de codigo abierto dedicado a determinar y
+ combatir las causas que hacen que el software sea inseguro
+
+ OSWAP top 10
+ 10 RIESGOS EN LAS APLICACIONES
+ los atacantes pueden utilizar diferentes rutas a traves de su
+ aplicacion para perjudicar su negocio u organizacion
+ TEORIA VER DIAPO
+ */
