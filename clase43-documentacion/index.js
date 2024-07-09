a47c46c5b80dbc7f8246eb8d2980f83bfb6b0a59
gomezclaudio11 <claudiogomez23@gmail.com>
2024-07-08 21:01:54 -0300

    clase 43 documentacion

---
+++ clase43-documentacion/index.js
@@ -0,0 +1,35 @@
+/**DOCUMENTACION EN API REST */
+/**
+ **API
+ interfaz de programacion de aplicaciones
+ ES un conjunto de definiciones y protocolos que se utiliza para
+ desarrollar e integrar el software de las aplicaciones
+ API REST
+ Un servicio REST no es una arquitectura software sino un 
+ conjunto de restricciones a tener en cuenta en la arquitectura 
+ software que usaremos para crear aplicaciones web respetando
+ HTTP
+ las restriccionesque definen a un sistema REST son
+ cliente-servidor
+ sin estado
+ cacheable
+ interfaz uniforme 
+ sistema de capas
+ */
+/**DOCUMENTACION DE UNA API */
+/**
+ Una documentacion bien estructurada facilita el proceso de 
+ aprendizaje sobre el nuevo entorno que se desarrollara 
+ basandose en tu servicio API
+
+ HERRAMIENTAS DE DOCUMENTACION API SWAGGER
+
+ Es una herramienta extremadamente util para describir producir
+ consumir y visualizar api restfull 
+ Su principal objetivo es unificar el sistema de documentacion
+ api con el propio codigo desarrollado para que este 
+ sincronizado en cada cambio
+ Con swagger documentamos nuestra api al mismo tiempo que
+ creamos una nueva implementacion
+ 
+ */
