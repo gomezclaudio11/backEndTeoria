53ede230f76a831e7b28da8851150a710f38e689
gomezclaudio11 <claudiogomez23@gmail.com>
2024-07-12 19:29:04 -0300

    clase 45 nestJs

---
+++ clase45-nestjs/index.js
@@ -0,0 +1,29 @@
+/**
+ NEST
+ Es un framework de node basado tanto en node como Express que
+ nos permite construir un backEnd en typescript o js con el
+ patron MVC
+ Unas de sus principales fortalezas es que ofrece un poderoso
+ marco de trabajo similar a otros frameworks MVC existentes
+ en otros lenguajes
+ Cuenta con lenguaje tipado, separación por módulos, 
+ generación automática de entidades a partir de una base de 
+ datos, ORM, construcción de endpoints a través de decoradores,
+ inyección de dependencias.
+NestJS está influenciado en gran medida por Angular, 
+aprovechando muchos de sus conceptos como son los módulos, 
+los controladores e inyección de dependencias.
+Cuenta con su propia CLI para generar y facilitarnos las 
+tareas de creación de todos estos elementos
+Nest CLI es una herramienta de interfaz de línea de comandos 
+que nos ayuda a inicializar, desarrollar y mantener nuestras 
+aplicaciones Nest.
+Ayuda de múltiples formas, incluido el andamiaje del proyecto, 
+el servicio en modo de desarrollo y la construcción y 
+agrupación de la aplicación para la distribución de producción. 
+Incorpora patrones arquitectónicos de mejores prácticas para 
+fomentar aplicaciones bien estructuradas.
+
+
+
+*/
